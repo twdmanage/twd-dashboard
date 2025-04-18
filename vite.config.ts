1e480ba959f89d887e3bcff6f73a59948bd9760f
twdmanage <denzel@thyswooddesign.com>
2025-04-18 20:32:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/[YOUR_REPOSITORY_NAME]/' : '/',
+  base: mode === 'production' ? '/twddashboard/' : '/',
   server: {
     host: "::",
     port: 8080,
